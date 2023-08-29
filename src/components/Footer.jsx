d56b19e115130cdb96b82185f801b9dacc567a0f
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-08-29 10:08:32 +0100

    Footer implementation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,49 +3,64 @@ import React from "react";
 const Footer = () => {
   return(
   <div>
-    <div className="bg-[#0C450C] text-white mt-[-1rem] font-montserrat">
-      
+      <div className="bg-[#0C450C] text-white mt- [-1rem] font-montserrat">
       <div className="flex justify-between align-center gap-10 px-24 py-20">
-        <div className="ml-[2rem]">
-            <p className="font-extrabold mb-2">Company</p>
-            <div className="text-gray-300 flex flex-col gap-1">
-            <p>About</p>
-            <p>Why Choose Us</p>
-            <p>Pricing</p>
-            <p>Testimonial</p>
-            </div>
-        </div>
+          <div className="ml-[2rem]">
+              <p className="font-extrabold mb-2">Company</p>
+              <div className="text-gray-300 flex flex-col gap-1">
+              <p>About</p>
+              <p>Why Choose Us</p>
+              <p>Pricing</p>
+              <p>Testimonial</p>
+              </div>
+          </div>
+          <div>
+              <p className="font-extrabold mb-2">Resources</p>
+              <div className="text-gray-300 flex flex-col gap-1">
+                <p>Privacy Policy</p>
+                <p>Terms and Condition</p>
+                <p>Blog</p>
+                <p>Contact Us</p>
+              </div>
+          </div>
+          <div>
+              <p className="font-extrabold mb-2">Product</p>
+              <div className="text-gray-300 flex flex-col gap-1">
+                <p>Micro Pension Scheme</p>
+                <p>Frequently Asked Questions</p>
+              </div>
+          </div>
+          <div>
+            <p className="font-extrabold text-4xl mb-5">Valubah</p>
+              <p className="mb-6">Subscribe to our Newsletter</p>
+              <div className="mt-6">
+                <input type="text" placeholder="Enter your email" className="h-[3rem] bg-[#2B2E3C]  text-grayy-300 p-2 hover:border-none focus:border-none active:border-none" /><span className="bg-[#FBCBC9] p-3 font-medium text-gray-700 rounded-sm">
+                Get Started
+                  </span>  
+              </div>
+          </div>
         <div>
-            <p className="font-extrabold mb-2">Resources</p>
-            <div className="text-gray-300 flex flex-col gap-1">
-              <p>Privacy Policy</p>
-              <p>Terms and Condition</p>
-              <p>Blog</p>
-              <p>Contact Us</p>
-            </div>
         </div>
-        <div>
-            <p className="font-extrabold mb-2">Product</p>
-            <div className="text-gray-300 flex flex-col gap-1">
-              <p>Micro Pension Scheme</p>
-              <p>Frequently Asked Questions</p>
-            </div>
         </div>
-        <div>
-          <p className="font-extrabold text-4xl mb-5">Valubah</p>
-            <p className="mb-6">Subscribe to our Newsletter</p>
-            <div className="mt-6">
-              <input type="text" placeholder="Enter your email" className="h-[3rem] bg-[#2B2E3C]  text-grayy-300 p-2 hover:border-none focus:border-none active:border-none" /><span className="bg-[#FBCBC9] p-3 font-medium text-gray-700 rounded-sm">
-              Get Started
-                </span>  
-            </div>
-        </div>
-        <div>
-
-        </div>
+      
+        {/* Social media section */}
+        <div className="mx-20 flex gap-4 mt-3">
+          <div className="w-[60%] h-[0.5px] bg-white"  ></div>
+          <div className="mb-6 mt-[-2rem]"> Copyright Valubah 2023
+            <span className="flex">
+              <img />
+              <img />
+              <img />
+              <img />
+          </span>
+          </div>
+          <div className="w-[60%] h-[0.5px] bg-white"></div>
         </div>
       </div>
-    </div>)
+
+
+  </div>
+)
 };
 
 export default Footer;

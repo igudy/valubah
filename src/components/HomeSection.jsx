b3266dcfec0a21e04efc41f2ea1ee20698baf323
igudy <goodnessigunma1@gmail.com>
2023-08-31 03:54:07 +0100

    Hero section fix

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -1,101 +1,98 @@
 import React from "react";
-import People2 from '../assets/people2.png'
-import People3 from '../assets/people3.png'
-import People4 from '../assets/people4.png'
-import Background2 from '../assets/background2.png'
-import Background3 from '../assets/background3.png'
+import People2 from "../assets/people2.png";
+import People3 from "../assets/people3.png";
+import People4 from "../assets/people4.png";
 
 const HomeSection = () => {
-  return     <>
-  {/* First Section */}
-  <div className='px-[30rem] font-montserrat'>
-    <div className='flex items-center text-center justify-center'>
-      <div className='mt-20'>
-        <p className='text-4xl font-bold'>
-          Tailored Financial Security for Every Stage
-        </p>
-        <p className='text-lg mb-20'>
-          Explore our range of comprehensive financial support and protection solutions designed for Gig Workers, Informal Income Earners, and Businesses.
-        </p>
-      </div>
-    </div> 
-  </div>
-
-  {/* Second Section */}
-  <div className='px-28'>
-    <div className='flex'>
-      <div className='basis-1/2 p-10'>
-        <img src={People2} />
-
-      </div>
-      <div className='basis-1/2 p-10 mt-[8rem]'>
-        <p className='text-2xl font-bold'>
-        Pension Plans
-        </p>
-        <p className='mt-6'>
-        Tailored for You Plan for a worry-free retirement with our customizable pension options, designed to provide you with long-term financial security and peace of mind
-        </p>
-      </div>
-    </div>
-  </div>
-
-
-
-  {/* Third Section */}
-  <div className='px-28'>
-    <div className='flex'>
-      <div className='basis-1/2 p-10 mt-[8rem]'>
-        <p className='text-2xl font-bold'>
-        Life Insurance
-        </p>
-        <p className='mt-6'>
-        Life Insurance That Matters Protect your loved ones' futures with our reliable life insurance coverage, ensuring financial stability even in unforeseen circumstances.
-        </p>
-      </div>
-
-      <div className='basis-1/2 p-10'>
-        <img src={People3} />
-      </div>
-    </div>
-  </div>
-
-  
-  {/* Fourth Section */}
-  <div className='px-28'>
-    <div className='flex'>
-      <div className='basis-1/2 p-10'>
-        <img src={People4} />
-
-      </div>
-      <div className='basis-1/2 p-10 mt-[8rem]'>
-        <p className='text-2xl font-bold'>
-        Health and Accident Protection
-        </p>
-        <p className='mt-6'>
-        You Can Count On Stay prepared for life's challenges with our comprehensive health and accident coverage, offering you the support you need when you need it most
-        </p>
-      </div>
-    </div>
-  </div>
-
-  {/* Fifth Section */}
-  <div className='px-[20rem]'>
-    <div className='flex items-center text-center justify-center bg-[#FBCBC9] rounded-xl'>
-      <div className='mt-10'>
-        <p className='text-4xl font-bold px-[6rem]'>
-        Join Now to Access Financial Confidence
-        </p>
-        <p className='mt-6 text-lg px-[14rem]'>
-        Join Valubah today and take the first step towards securing your future..
-        </p>
-        <div className='flex items-center text-center justify-center'>
-          <p className="bg-[#146614] py-4 w-[12rem] mt-6 font-bold text-white rounded-lg mb-10">Get Started</p>
+  return (
+    <>
+      {/* First Section */}
+      <div className="px-[30rem] font-montserrat">
+        <div className="flex items-center text-center justify-center">
+          <div className="mt-20">
+            <p className="text-4xl font-bold">
+              Tailored Financial Security for Every Stage
+            </p>
+            <p className="text-lg mt-6 mb-7">
+              Explore our range of comprehensive financial support and
+              protection solutions designed for Gig Workers, Informal Income
+              Earners, and Businesses.
+            </p>
+          </div>
         </div>
       </div>
-    </div> 
-  </div>
 
-</>;
+      <div className="mx-28">
+        {/* Second Section */}
+        <div className="flex mt-20">
+          <div className="basis-[55%]">
+            <img src={People2} />
+          </div>
+          <div className="basis-[45%] p-10 mt-[8rem] mr-10">
+            <p className="text-2xl font-bold">Pension Plans</p>
+            <p className="mt-6">
+              Tailored for You Plan for a worry-free retirement with our
+              customizable pension options, designed to provide you with
+              long-term financial security and peace of mind
+            </p>
+          </div>
+        </div>
+
+        {/* Third Section */}
+        <div className="flex mt-20">
+          <div className="basis-[45%] p-10 mt-[8rem]">
+            <p className="text-2xl font-bold">Life Insurance</p>
+            <p className="mt-6">
+              Life Insurance That Matters Protect your loved ones' futures with
+              our reliable life insurance coverage, ensuring financial stability
+              even in unforeseen circumstances.
+            </p>
+          </div>
+
+          <div className="basis-[55%]">
+            <img src={People3} />
+          </div>
+        </div>
+
+        {/* Fourth Section */}
+        <div className="flex mt-20">
+          <div className="basis-[55%]">
+            <img src={People4} />
+          </div>
+          <div className="basis-[45%] p-10 mt-[8rem]">
+            <p className="text-2xl font-bold">Health and Accident Protection</p>
+            <p className="mt-6">
+              You Can Count On Stay prepared for life's challenges with our
+              comprehensive health and accident coverage, offering you the
+              support you need when you need it most
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Fifth Section */}
+      <div className="px-[20rem] mt-[5rem]">
+        <div className="flex items-center text-center justify-center">
+          <div className="pink-div h-[80%] w-[100%] rounded-xl">
+            <div className="mt-10">
+              <p className="text-4xl font-bold px-[6rem]">
+                Join Now to Access Financial Confidence
+              </p>
+              <p className="mt-6 text-lg px-[14rem]">
+                Join Valubah today and take the first step towards securing your
+                future..
+              </p>
+              <div className="flex items-center text-center justify-center">
+                <p className="bg-[#146614] py-4 w-[12rem] mt-6 font-bold text-white rounded-lg mb-10">
+                  Get Started
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HomeSection;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,48 +1,54 @@
-import React, { useState } from "react"
-import People from '../assets/people.png'
+import React, { useState } from "react";
+import People from "../assets/people.png";
 
 const Navbar = () => {
-  const [toggle, setToggle] = useState(false)
+  const [toggle, setToggle] = useState(false);
 
-  return (<div className="bg-[#146614] font-montserrat">
-    <div className="px-28">
-
-    <div className="w-full pt-6 flex justify-between items-center text-white">
-        <div>
-          <h1 className="text-[32px] font-extrabold">Valubah</h1>
+  return (
+    <div className="image-div h-[100vh] w-full font-montserrat">
+      <div className="px-28">
+        <div className="w-full pt-6 flex justify-between items-center text-white">
+          <div>
+            <h1 className="text-[32px] font-extrabold">Valubah</h1>
+          </div>
+          <div>
+            <ul className="flex gap-10">
+              <li>About</li>
+              <li>FAQs</li>
+              <li>Contact Us</li>
+              <li className="bg-[#FBCBC9] p-4 mt-[-15px] font-bold text-black rounded-lg">
+                Get Started
+              </li>
+            </ul>
+          </div>
         </div>
-        <div>
-          <ul className="flex gap-10">
-            <li>About</li>
-            <li>FAQs</li>
-            <li>Contact Us</li>
-            <li className="bg-[#FBCBC9] p-4 mt-[-15px] font-bold text-black rounded-lg">Get Started</li>
-          </ul>
+
+        {/* Write up and image */}
+        <div className="py-2 flex text-white">
+          <div className="basis-1/2">
+            <p
+              className="text-[50px] mt-24 leading-[4rem]
+        font-black"
+            >
+              <span className="text-[#FBCBC9]">Valubah: </span>Your Secure Path
+              to a Confident Future
+            </p>
+            <p className="mt-10 text-xl">
+              Explore our Comprehensive Financial Support and Protection
+              Solutions Tailored for Gig Workers, Informal Income Earners, and
+              Businesses.
+            </p>
+            <p className="bg-[#FBCBC9] pl-12 py-4 w-[12rem] mt-10 font-bold text-black rounded-lg">
+              Get Started
+            </p>
+          </div>
+          <div className="basis-1/2">
+            <img src={People} className="pl-10 ml-10" />
+          </div>
         </div>
-    </div>
-    
-    {/* Write up and image */}
-    <div className="py-2 flex text-white">
-      <div className="basis-1/2">
-        <p className="text-[50px] mt-24 leading-[4rem]
-        font-black"><span className="text-[#FBCBC9]"> 
-          Valubah: {" "}
-        </span>Your 
-        Secure Path to a 
-      Confident Future</p>
-        <p className="mt-10 text-xl">Explore our Comprehensive Financial Support and
-        Protection Solutions Tailored for Gig Workers,
-            Informal Income Earners, and Businesses.</p>
-          <p className="bg-[#FBCBC9] pl-12 py-4 w-[12rem] mt-10 font-bold text-black rounded-lg">Get Started</p>
-      </div>
-      <div className="basis-1/2">
-        <img src={People} className="pl-10 ml-10"/>
       </div>
     </div>
+  );
+};
 
-    </div>
-    </div>
-  )
-}
-
-export default Navbar
+export default Navbar;
